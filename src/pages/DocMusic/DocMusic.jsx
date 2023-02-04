5c30287938b38de1daa62f5831949ab9001f3fe4
Victor Manuel Castro Lopez <vicbeatles@live.com.mx>
2023-02-04 03:57:58 -0600

    PhotoDocument layouts done

--- src/pages/DocMusic/DocMusic.jsx
+++ src/pages/DocMusic/DocMusic.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './DocMusic.css'
 
 const DocMusic = (props) => {
 
   const {setWhere} = props;
-  useEffect(()=> {setWhere('photo-documental-item')})
+  const [transition, setTransition] = useState(false)
+
+  useEffect(()=> {
+    setWhere('photo-documental-item')
+    setTimeout(()=> {setTransition(true)}, 100)
+  })
   
   return (
-    <div class='music-container'>
+    <div className={!transition ? 'music-container' : 'music-container music-container-t'}>
       <div class='music-photo'></div>
       <div class='music-footer'>MUSICA DE CIEGOS</div>
       <div class='music-desc'><p>Fujifilm 2021</p><p>MINERAL DEL CHICO, HIDALGO, MEXICO</p></div>
