b7f2062033fed948455d7c1cd7667f1d58eb5fc0
Victor Manuel Castro Lopez <vicbeatles@live.com.mx>
2023-02-10 01:39:08 -0600

    Layout bug fixes

--- src/pages/Ux/Ux.jsx
+++ src/pages/Ux/Ux.jsx
@@ -24,15 +24,11 @@ const Ux = (props) => {
         <div class='photo7-ux'></div>
         <div class='photo8-ux'></div>
         <div class='photo9-ux'></div>
+        <div class='photo10-ux'></div>
         <div class='video1-ux'><video src={video1} loop autoPlay muted></video></div>
         <div class='video2-ux'><video src={video2} loop autoPlay muted></video></div>
-        <div class='frame-te'>
-        <iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FwIcvjggBIyBb2HrIXjBE4k%2FUntitled%3Fnode-id%3D0%253A1%26t%3DKBfLEm41VDaGqv27-1" allowfullscreen title='FIGMA'></iframe>
-        </div>
         <div class='description-ux'><h1 class='product-desc-title'>UX</h1><p>Gifs mostrando el funcionamiento de algunos botones y 
             animaciones hechas en Figma para diseño UX.</p></div>
-        <div class='description-soyte'><h1 class='product-desc-title'>Solo Té</h1><p>Proyecto realizado para mostrar las habilidades en 
-          UX y los programas de esta rama.</p></div>
     </div>
   )
 }
